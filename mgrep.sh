4c60fe8c7062c92817c479791486fef2690793d0
Remi DROUIN <rdrouin@graimatterlabs.ai>
2021-01-07 16:33:29 +0100

    fix PATTERN + nobin

--- mgrep.sh
+++ mgrep.sh
@@ -7,6 +7,9 @@ EXCLUDE_FILE_ARRAY=()
 EXCLUDE_STRING=""
 INCLUDE_FILE_ARRAY=()
 INCLUDE_STRING=""
+EXCLUDE=0
+CASE=1
+BIN=1
 
 usage()
 {
@@ -19,51 +22,55 @@ if [[ ${#args[*]} -lt 2 ]] ; then
     exit 1
 fi
 
-PATTERN=""
-EXCLUDE=0
-CASE=1
 
-LENGTH=$((${#args[*]}))
+
+LENGTH=$((${#args[*]} - 1))
 for i in $(seq 1 $LENGTH) ; do
-    case ${args[$i]} in
-        -h|--help) usage
-            exit 0;;
-        -v|--version) echo "1"
-            exit 0;;
-        -e|--exclude)
-            EXCLUDE=1
-            INCLUDE=0;;
-        -i|--include)
-            INCLUDE=1
-            EXCLUDE=0;;
-        --case)
-            CASE=0;;
-        *)
-            if [[ $EXCLUDE -eq 1 ]] ; then
-                EXCLUDE_FILE_ARRAY+=(${args[$i]})
-                EXCLUDE_STRING+="| grep -v \"${args[$i]}\""
-            elif [[ $INCLUDE -eq 1 ]] ; then
-                INCLUDE_FILE_ARRAY+=(${args[$i]})
-                INCLUDE_STRING+="| grep \"${args[$i]}\""
-            else
-                if [[ ${PATTERN} = "" ]] ; then
-                    PATTERN=${args[$i]}
+    if [[ ${args[$i]} != "" ]] ; then
+        case ${args[$i]} in
+            -h|--help) usage
+                exit 0;;
+            -v|--version) echo "1"
+                exit 0;;
+            -e|--exclude)
+                EXCLUDE=1
+                INCLUDE=0;;
+            -i|--include)
+                INCLUDE=1
+                EXCLUDE=0;;
+            --case)
+                CASE=0;;
+            --nobin)
+                BIN=0;;
+            *)
+                if [[ $EXCLUDE -eq 1 ]] ; then
+                    EXCLUDE_FILE_ARRAY+=(${args[$i]})
+                    EXCLUDE_STRING+="| grep -v \"${args[$i]}\""
+                elif [[ $INCLUDE -eq 1 ]] ; then
+                    INCLUDE_FILE_ARRAY+=(${args[$i]})
+                    INCLUDE_STRING+="| grep \"${args[$i]}\""
                 else
-                    echo "PATTERN already given"
-                    exit 1
+                    if [[ ${PATTERN} = "" ]] ; then
+                        PATTERN=${args[$i]}
+                    else
+                        echo "PATTERN already given"
+                        exit 1
+                    fi
                 fi
-            fi
-            ;;
-    esac
+                ;;
+        esac
+    fi
 done
 
+GREP_OPT="-Rl"
 if [[ ${CASE} -eq "1" ]] ; then
-    cmd="grep -Ril ${PATTERN}"
-else
-    cmd="grep -Rl ${PATTERN}"
+    GREP_OPT+="i"
+fi
+if [[ ${BIN} -eq "0" ]] ; then
+    GREP_OPT+="I"
 fi
 
-# for f in $(grep -Ril "${PATTERN}") ; do
+cmd="grep ${GREP_OPT} $PATTERN"
 for f in $($cmd) ; do
     valid=1
     for exclude_f in ${EXCLUDE_FILE_ARRAY[@]} ; do
